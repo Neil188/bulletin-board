fdd2baacb5c6168d2f364c8a59a8c84ec21796c2
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-11-02 11:06:25 +0000

    Updates

---
+++ precache-manifest.bdca4f7720ead0cabd14f4c0cd7a4938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "815a3d4b338105962b38",
+    "url": "/bulletin-board/static/js/runtime~main.815a3d4b.js"
+  },
+  {
+    "revision": "067fb2b4c0c1e976ee83",
+    "url": "/bulletin-board/static/js/main.067fb2b4.chunk.js"
+  },
+  {
+    "revision": "137635441adf60fd62ee",
+    "url": "/bulletin-board/static/js/1.13763544.chunk.js"
+  },
+  {
+    "revision": "067fb2b4c0c1e976ee83",
+    "url": "/bulletin-board/static/css/main.9732e1c1.chunk.css"
+  },
+  {
+    "revision": "2a699534d291a722109ff02e1e98b429",
+    "url": "/bulletin-board/index.html"
+  }
+];
